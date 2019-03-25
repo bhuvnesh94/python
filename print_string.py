bdd6baa017d79a074443a125984c2d3276ee3a5b
bhuvnesh94 <bhuvnesh1510@gmail.com>
2019-03-25 15:13:52 +0530

    python program for print function and strings

---
+++ print_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 14:14:00 2019
+
+@author: bhanu
+"""
+
+#printing hello world, using single inverted commas it will print it as it is and treat is as string. 
+print('Hello World')
+
+#use of concatenation 
+print('testing','concatenation') #we can use ',' for concatenation and this separator provides a space between strings
+print('another'+'testing') #we canuse '+' for concatenation but it doesnot provide space between strings
+print('welcome'+'5')
+#in below either we start with '' or "" but can be used once at outside and another one can be used multiple times.
+print('welcome "hello" "sure"')
+print("welcome 'hello'")
+#we can use '\' for printing '' single quotes in single quotes
+#bcoz backslash act as a escape character
+print('hello\n\'hello\'')
