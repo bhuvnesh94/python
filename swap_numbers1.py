fe5896bc11b8a3015655b27ca4bc64a043ccb2af
Bhuvnesh Sharma <bhuvnesh1510@gmail.com>
2019-07-09 16:28:44 +0530

    swapping two numbers without using temporary variable

---
+++ swap_numbers1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#python program to swap two numbers using third variable.
+#taking two numbers from user
+num1= input("enter 1st number: ")
+num2= input("enter 2nd number: ")
+#swapping two numbers without using temporary variable
+num1, num2 = num2, num1
+#printing numbers after swaping 
+print("num1="+num1)
+print("num2="+num2)
